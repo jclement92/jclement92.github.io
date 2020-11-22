9f90a815dcd944619e8ffe9fd7a40ab56512852f
jclement92 <jclement92@yahoo.com>
2020-11-22 11:39:49 -0500

    Card subtitles, links

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
-import Ripples from 'react-ripples'
+// import Ripples from 'react-ripples'
 // import Form from 'react-bootstrap/Form';
 
 const Projects = () => {
@@ -25,7 +25,7 @@ const Projects = () => {
 						<option value="3">Instagram Clone</option>
 					</Form.Control>
 				project? That's interesting.
-				&nbsp;
+				{' '}
 				<Button type="submit" className="my-1">
 						Learn more
   				</Button>
@@ -39,15 +39,17 @@ const Projects = () => {
 							<Card.Img variant="top" src="https://holder.ninja/400x300.svg" />
 							<Card.Body>
 								<Card.Title>Voice VR</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">Unity</Card.Subtitle>
 								<Card.Text>
 									Experience Virtual Reality! Use your voice to interact with content.
-			</Card.Text>
+								</Card.Text>
 							</Card.Body>
 						</Card>
 						<Card>
 							<Card.Img variant="top" src="https://holder.ninja/400x300.svg" />
 							<Card.Body>
 								<Card.Title>Florida Man Challenge App</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">Android (Java)</Card.Subtitle>
 								<Card.Text>
 									Learn about Florida's craziest stories. Enter your birthday and share the results with your friends, too!
 								</Card.Text>
@@ -57,12 +59,13 @@ const Projects = () => {
 							<Card.Img variant="top" src="https://holder.ninja/400x300.svg" />
 							<Card.Body>
 								<Card.Title>Instagram Clone</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">Android (Java)</Card.Subtitle>
 								<Card.Text>
 									Instagram, Part II!
 									<br />
 									<br />
 									<small>Not affiliated with the real Instagram in any way. This project was used for educational purposes.</small>
-			</Card.Text>
+								</Card.Text>
 							</Card.Body>
 						</Card>
 					</CardDeck>
@@ -73,27 +76,30 @@ const Projects = () => {
 							<Card.Img src="https://holder.ninja/400x300.svg" />
 							<Card.Body>
 								<Card.Title>Library Database</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">HTML, CSS, JavaScript, PostgreSQL, Tomcat Server, Java</Card.Subtitle>
 								<Card.Text>
 									Ever wanted to keep track of your books? Enter the Library Database!
-			</Card.Text>
+								</Card.Text>
 							</Card.Body>
 						</Card>
 						<Card>
 							<Card.Img variant="top" src="https://holder.ninja/400x300.svg" />
 							<Card.Body>
 								<Card.Title>Notaker</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">Android (Kotlin)</Card.Subtitle>
 								<Card.Text>
 									Take notes on the go
-			</Card.Text>
+								</Card.Text>
 							</Card.Body>
 						</Card>
 						<Card>
 							<Card.Img variant="top" src="https://holder.ninja/400x300.svg" />
 							<Card.Body>
 								<Card.Title>FriendzOnly</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">React Native</Card.Subtitle>
 								<Card.Text>
 									A Social App that brings friends together
-					</Card.Text>
+								</Card.Text>
 							</Card.Body>
 						</Card>
 					</CardDeck>
@@ -104,18 +110,23 @@ const Projects = () => {
 							<Card.Img src="https://holder.ninja/400x300.svg" />
 							<Card.Body>
 								<Card.Title>Covid-19 South Florida</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">Python (3)</Card.Subtitle>
 								<Card.Text>
 									Keep track of Covid-19 cases in South Florida. Learn about the latest hotspots in the area.
-			</Card.Text>
+								</Card.Text>
+								<Card.Link href="https://github.com/jclement92/COVID-19-South-Florida">See it on GitHub</Card.Link>
 							</Card.Body>
 						</Card>
 						<Card>
 							<Card.Img variant="top" src="https://holder.ninja/400x300.svg" />
 							<Card.Body>
-								<Card.Title>Easter Bunny Egg Hunt</Card.Title>
+								<Card.Title>Easter Bunny Egg Hunt Mobile</Card.Title>
+								<Card.Subtitle className="mb-2 text-muted">Android (Java)</Card.Subtitle>
 								<Card.Text>
-									Cute game involving bunnies in a house
-			</Card.Text>
+									Cute game involving bunnies in a house. Collect as many items as you can!
+								</Card.Text>
+								{/* <Card.Link>Learn more</Card.Link> */}
+								<Card.Link href="https://github.com/jclement92/Easter-Bunny-Egg-Hunt-Mobile">See it on GitHub</Card.Link>
 							</Card.Body>
 						</Card>
 						<Card>
@@ -124,14 +135,12 @@ const Projects = () => {
 								<Card.Title>Movies Api (Future)</Card.Title>
 								<Card.Text>
 									Coming soon!
-			</Card.Text>
+								</Card.Text>
 							</Card.Body>
-						</Card>
-						
+						</Card>	
 					</CardDeck>
 				</Card>
 			</div>
-
 		</div>
 	);
 }
